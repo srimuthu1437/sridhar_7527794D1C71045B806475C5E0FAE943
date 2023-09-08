09edbb5378e105588fbde6195e535c4e3a27493d
srimuthu1437 <144457948+srimuthu1437@users.noreply.github.com>
2023-09-08 19:08:48 +0530

    Add files via upload

---
+++ CHALLANGE 1/main (1).py
@@ -0,0 +1,12 @@
+def factorial(n):
+  if n == 0:
+    return 1
+  else:
+    return n * factorial(n - 1)
+print(factorial(5))
+def factorial(n):
+      if n == 0:
+        return 1
+      else:
+        return n * factorial(n - 1)
+        print(factorial(5))
